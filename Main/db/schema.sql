bc3cf0ef6f0e0d2425f0cbadf33a44f0b1e4f21e
Alexandra Capodilupo <acapodilupo21@aol.com>
2021-03-27 11:53:13 -0400

    updating handlebars

--- Main/db/schema.sql
+++ Main/db/schema.sql
@@ -1,2 +1,14 @@
-DROP DATABASE IF EXISTS crowdfund_db;
-CREATE DATABASE crowdfund_db;
+DROP DATABASE IF EXISTS active_userDB;
+CREATE database active_userDB;
+
+use  active_userDB;
+
+CREATE TABLE user(
+id INT AUTO_INCREMENT NOT NULL,
+firstName VARCHAR(50),
+lastName VARCHAR(75),
+activity VARCHAR(25),
+email VARCHAR(50),
+password VARCHAR(10),
+Primary Key(id)
+);
