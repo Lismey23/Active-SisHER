4a1057605e03b0cc8457659c02081a233d6e2dbe
Ree Britt <74746244+reebritt@users.noreply.github.com>
2021-03-30 20:35:31 -0400

    updated routes and handlebars

--- Main/public/js/login.js
+++ Main/public/js/login.js
@@ -2,14 +2,17 @@ const loginFormHandler = async (event) => {
   event.preventDefault();
 
   // Collect values from the login form
-  const email = document.querySelector('#email-login').value.trim();
-  const password = document.querySelector('#password-login').value.trim();
+  const firstName = document.querySelector('#firstName-signup').value.trim();
+  const lastName = document.querySelector('#lastName-signup').value.trim();
+  const activity = document.querySelector('#activity-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
 
   if (email && password) {
     // Send a POST request to the API endpoint
     const response = await fetch('/api/users/login', {
       method: 'POST',
-      body: JSON.stringify({ email, password }),
+      body: JSON.stringify({ firstName, lastName, activity, email, password }),
       headers: { 'Content-Type': 'application/json' },
     });
 
@@ -25,14 +28,17 @@ const loginFormHandler = async (event) => {
 const signupFormHandler = async (event) => {
   event.preventDefault();
 
-  const name = document.querySelector('#name-signup').value.trim();
+  
+  const firstName = document.querySelector('#firstName-signup').value.trim();
+  const lastName = document.querySelector('#lastName-signup').value.trim();
+  const activity = document.querySelector('#activity-signup').value.trim();
   const email = document.querySelector('#email-signup').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
 
-  if (name && email && password) {
+  if (firstName && lastName && activity && email && password) {
     const response = await fetch('/api/users', {
       method: 'POST',
-      body: JSON.stringify({ name, email, password }),
+      body: JSON.stringify({ firstName, lastName, activity, email, password }),
       headers: { 'Content-Type': 'application/json' },
     });
 
