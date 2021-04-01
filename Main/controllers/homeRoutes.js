7c0884fa913fb265eab4b6c012d398dbf198bc96
Ree Britt <74746244+reebritt@users.noreply.github.com>
2021-03-31 20:03:24 -0400

    Update homeRoutes.js

--- Main/controllers/homeRoutes.js
+++ Main/controllers/homeRoutes.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
-const { Project, User } = require('../models');
+const { User } = require('../models');
 const withAuth = require('../utils/auth');
 
+router.get('/', async (req, res) => {
+  res.render("homepage")
+})
 router.get('/', async (req, res) => {
   try {
     // Get all projects and JOIN with user data
@@ -27,47 +30,8 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/project/:id', async (req, res) => {
-  try {
-    const projectData = await Project.findByPk(req.params.id, {
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
 
-    const project = projectData.get({ plain: true });
 
-    res.render('project', {
-      ...project,
-      logged_in: req.session.logged_in
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-// Use withAuth middleware to prevent access to route
-router.get('/profile', withAuth, async (req, res) => {
-  try {
-    // Find the logged in user based on the session ID
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-      include: [{ model: Project }],
-    });
-
-    const user = userData.get({ plain: true });
-
-    res.render('profile', {
-      ...user,
-      logged_in: true
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
 
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
